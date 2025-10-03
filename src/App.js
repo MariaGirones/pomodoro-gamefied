4015b162465d0b7da84c70e06d06d3bd97b75dd9
MariaGirones <mariagirones.me@gmail.com>
2025-10-03 11:37:04 -0600

    importamos useState e hicimos funcion de startTimer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { useState } from 'react'; // ✅ Importamos useState
 
 function App() {
+  //agrego 3 lineas nuevas
+  const [timeLeft, setTimeLeft] = useState(25 * 60); // sin tiempo no hay pomodoro
+  
+  const startTimer = () => {
+    // Lógica para iniciar el temporizador
+  };
   return (
     <div className="App">
        <h1>🍅 NeuroStudy Quest</h1>  {/* ✅ Nuestro título personalizado */}
       <p>Tu RPG de Pomodoros</p>  {/* ✅ Nuestra descripción */}
     </div>
   );
+
+  // fin de las 3 lineas nuevas
 }
 
 export default App;
